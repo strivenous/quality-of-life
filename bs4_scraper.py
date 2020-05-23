999d8c677c1f8e6e1d608cba46ad813368f7a985
strivenous <65793524+strivenous@users.noreply.github.com>
2020-05-23 22:34:46 +0200

    Update bs4_scraper.py

--- bs4_scraper.py
+++ bs4_scraper.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from strivencore import *
+import requests
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+webhook = DiscordWebhook(url='insert webhook link here')
+playersformat = DiscordWebhook(url=webhook, content='Webhook Message') # @TODO add players list content
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, lxml)
+
+
+"""
+vars taken from strivencore
+
+"""
